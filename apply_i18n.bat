6181f4185a9060944364aeb3391b77ac6f744c0f
CIbeaST <61186415+CIbeaST@users.noreply.github.com>
2020-02-18 14:17:59 +0300

    Update apply_i18n.bat

--- apply_i18n.bat
+++ apply_i18n.bat
@@ -1,2 +1,2 @@
 @echo on
-python tools\apply_i18n.py -d Data\dialogs\Data\Dialogs -T data\i18n -o data\dialogs\ -S
+python tools\apply_i18n.py -d ..\data\dialogs -T ..\data\i18n -o data_new\dialogs\ -S
