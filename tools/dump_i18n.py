6ee5a3f70f77ded55f241d9d7dacdf53c2ac9f9b
CIbeaST <61186415+CIbeaST@users.noreply.github.com>
2020-02-18 14:29:48 +0300

    fix ext

--- tools/dump_i18n.py
+++ tools/dump_i18n.py
@@ -19,7 +19,7 @@ def unpack(entry, opts):
         i18n_path, file_name = store_path.rsplit('/', 1)
         if not os.path.exists(i18n_path):
             os.makedirs(i18n_path)
-        file_name = file_name.replace('.udlg', '.txt')
+        file_name = file_name+'.txt'
         store_path = os.path.join(i18n_path, file_name)
         if not(opts.skip_processed and os.path.exists(store_path)):
             print("Processing: %s" % entry.path)
