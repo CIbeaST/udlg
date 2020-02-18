708e5014874a4524da10559f3441d980cdfa181b
CIbeaST <61186415+CIbeaST@users.noreply.github.com>
2020-02-18 14:22:26 +0300

    fix win

--- apply_i18n.bat
+++ apply_i18n.bat
@@ -1,2 +1,2 @@
 @echo on
-python tools\apply_i18n.py -d ..\data\dialogs -T ..\data\i18n -o data_new\dialogs\ -S
+python tools\apply_i18n.py -d ..\data\dialogs -T ..\data\i18n\dialogs -o ..\data_new\dialogs\ -S
